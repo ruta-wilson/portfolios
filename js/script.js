243e4df5e26ce2d5fc2627352e1232e21627ae28
RUTAREMARA Wilson <rutaremarawilson@gmail.com>
2025-02-28 17:32:21 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// You can add JavaScript here for interactivity.
+// For example, smooth scrolling:
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
